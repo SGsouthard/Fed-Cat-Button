013dc79a508e9ba7e14cd9e0a71b7b79454b3e71
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-02 02:50:55 -0700

    Cookie struggles in JS

--- bits.js
+++ bits.js
@@ -7,21 +7,12 @@ let catFed = "";
 //setting up a cookie to store the current date/time
 //Time Cookie should be: timestamp = timeString
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie= cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    
-}
 
 feedButton.onclick = function() {
     let timeTime = new Date();
     let timeString = timeTime.toLocaleTimeString();
     timeBlock.textContent = timeString;
+    
 }
 
 console.log("JS is connected and online!");
