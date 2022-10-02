f0e81ac94df2449cc9771109a9631a532fb4af0a
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-01 21:20:04 -0700

    updates to the JS file

--- bits.js
+++ bits.js
@@ -1,3 +1,17 @@
+let feedButton = document.getElementById("feed-button");
+let timeBlock = document.getElementById("time-block");
+//Time Section
+let currentDate = new Date();
+let timeNow = + currentDate.getHours() + ":"
+    + currentDate.getMinutes() + ":"
+    + currentDate.getSeconds()
+// function getTime() {
 
+// }
+
+feedButton.onclick = function() {
+    console.log(timeNow);
+    timeBlock.textContent = Date();
+}
 
 console.log("JS is connected and online!")
