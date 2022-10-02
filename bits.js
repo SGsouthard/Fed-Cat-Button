8d6d5d8e79a0dde8125f46a4d2f73b6e9ced5d05
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-02 01:43:45 -0700

    update JS

--- bits.js
+++ bits.js
@@ -1,13 +1,27 @@
 let feedButton = document.getElementById("feed-button");
 let timeBlock = document.getElementById("time-block");
+let catSpan = document.getElementById("cat-span")
 //an empty string variable for storing the last date the cat was fed?
 let catFed = "";
+
 //setting up a cookie to store the current date/time
-//HOW
+//Time Cookie should be: timestamp = timeString
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie= cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    
+}
 
 feedButton.onclick = function() {
-    console.log(Date());
-    timeBlock.textContent = "this will be a date/time someday"
+    let timeTime = new Date();
+    let timeString = timeTime.toLocaleTimeString();
+    timeBlock.textContent = timeString;
 }
 
 console.log("JS is connected and online!");
