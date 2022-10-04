d1d12a3f3e8e79dc6634a293fac89c4ca15d1ea0
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-03 20:31:07 -0700

    Update HTML and JS
    added a 'delete' button to clear the local storage for the page
    
    updated the JS so the timestamp updates in real time with button presses

--- bits.js
+++ bits.js
@@ -1,10 +1,11 @@
 let feedButton = document.getElementById("feed-button");
+let deleteButton = document.getElementById("delete-data");
 let timeBlock = document.getElementById("time-block");
 let catSpan = document.getElementById("cat-span")
 
 
 
-//Local Storage set up?
+//Local Storage variable to load on the main page
 let catFed = localStorage.getItem('timeFed');
 
 window.onload = function() {
@@ -12,12 +13,26 @@ window.onload = function() {
 }
 
 feedButton.onclick = function() {
+    updateTimeStamp();
+    /*local storage variable so that the time can be updated
+    *each time the button is clicked on the page AND in the
+    *storage
+    */
+    let timeStamp = localStorage.getItem('timeFed');
+    timeBlock.textContent = timeStamp;
+}
+
+//delete button for development purposes only
+// deleteButton.onclick = function() {
+//     localStorage.clear();
+//     timeBlock.textContent = "";
+// }
+
+//function to update and store the Time Stamp in local storage
+function updateTimeStamp() {
     let timeTime = new Date();
     let timeString = timeTime.toLocaleTimeString();
     localStorage.setItem('timeFed',timeString);
-    timeBlock.textContent = catFed;
 }
-
-
 
 console.log("JS is connected and online!");
