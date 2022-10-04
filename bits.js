2a14c7ca69e202312f9370182eaa91162cd2e22e
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-10-03 19:41:33 -0700

    Update bits.js

--- bits.js
+++ bits.js
@@ -1,25 +1,21 @@
 let feedButton = document.getElementById("feed-button");
 let timeBlock = document.getElementById("time-block");
 let catSpan = document.getElementById("cat-span")
-//an empty string variable for storing the last date the cat was fed?
+
 
 
 //Local Storage set up?
-localStorage.setItem('myCat','RePete');
 let catFed = localStorage.getItem('timeFed');
+
+window.onload = function() {
+    timeBlock.textContent = catFed
+}
 
 feedButton.onclick = function() {
     let timeTime = new Date();
     let timeString = timeTime.toLocaleTimeString();
-    timeBlock.textContent = timeString;
-    localStorage.setItem('timeFed',timeString)
-}
-
-function checkTime() {
-    //this checks if there is already a time stamp made
-    if (catFed != "") {
-        console.log()
-    }
+    localStorage.setItem('timeFed',timeString);
+    timeBlock.textContent = catFed;
 }
 
 
